c6627a8a5ef6780c574fe6c5eaa2c02091772922
Suna <75311863+sunasaribas@users.noreply.github.com>
2021-11-14 12:18:06 +0300

    Harf notu

---
+++ HarfNotu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HarfNotu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            n = Convert.ToInt32("Öğrenci sayısını giriniz..");
+
+            for (int i =0; i<n; i++)
+            {
+                Console.WriteLine($"{i + 1}. öğrenci vizesi");
+                float vize = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine($"{i + 1}. öğrenci finali");
+                float final = Convert.ToSingle(Console.ReadLine());
+
+                float ortalama = (vize * 0.4f + final * 0.6f);
+
+                Console.WriteLine($"Öğrenci ortalaması = {ortalama}");
+            }
+             Console.ReadKey();
+        }
+    }
+}
